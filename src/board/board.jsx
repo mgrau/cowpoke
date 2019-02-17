ec954b203f0819d2da4230bbae991f0240906784
Matt Grau <matt.grau@gmail.com>
2019-02-18 00:45:47 +0100

    very simple game board

--- src/board/board.jsx
+++ src/board/board.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 export default class CowpokeBoard extends React.Component {
-  onClick(id) {
-    if (this.isActive(id)) {
-      this.props.moves.clickCell(id);
-      this.props.events.endTurn();
-    }
+  onClick(name) {
+    this.props.moves.move(name);
+    // this.props.moves.clickCell(id);
+    // this.props.events.endTurn();
   }
 
   isActive(id) {
@@ -34,13 +33,23 @@ export default class CowpokeBoard extends React.Component {
     };
 
     let tbody = [];
-    for (let i = 0; i < 3; i++) {
+
+    for (let i = 0; i < 7; i++) {
       let cells = [];
-      for (let j = 0; j < 3; j++) {
-        const id = 3 * i + j;
+      for (let j = 0; j < 7; j++) {
+        const index = 48 - (j + 7 * i);
+        const cellName = this.props.G.trail.trail[index].name;
+        const players = Object.values(this.props.G.players)
+          .filter(player => player.location == cellName)
+          .map(player => "P" + player.playerID);
         cells.push(
-          <td style={cellStyle} key={id} onClick={() => this.onClick(id)}>
-            {i},{j}
+          <td
+            style={cellStyle}
+            key={index}
+            onClick={() => this.onClick(cellName)}
+          >
+            <div>{cellName}</div>
+            <div>{players}</div>
           </td>
         );
       }
