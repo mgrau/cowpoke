409639053471f402e776f16ef8bcae40d5724c28
Matt Grau <matt.grau@gmail.com>
2019-02-18 05:02:03 +0100

    added react-lineto to draw lines between spaces

--- src/board/board.jsx
+++ src/board/board.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Tile from "./tile";
 
+import LineTo from "react-lineto";
+
 import "./board-style.css";
 
 export default class CowpokeBoard extends React.Component {
@@ -8,8 +10,23 @@ export default class CowpokeBoard extends React.Component {
     const spaces = this.props.G.trail.trail.map((space, index) => (
       <Tile key={index} {...space} G={this.props.G} moves={this.props.moves} />
     ));
+
+    const lines = this.props.G.trail.trail
+      .map(space =>
+        space.prev.map(prevSpace => (
+          <LineTo
+            from={space.name}
+            to={prevSpace}
+            borderColor="black"
+            borderWidth={3}
+            key={"line" + space.name + prevSpace}
+          />
+        ))
+      )
+      .flat();
     return (
       <div>
+        <div>{lines}</div>
         <div id="board">{spaces}</div>
 
         <div>Remaining moves: {this.props.G.movesRemaining}</div>
