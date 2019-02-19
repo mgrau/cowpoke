7a23d9e85f51a3e118ea2a3772676fd949b7c95e
Matt Grau <matt.grau@gmail.com>
2019-02-19 02:26:16 +0100

    add todo

---
+++ TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+- fix rendering of graph lines (they only present on a redraw)
+- break out css into separate style sheets for different components
--- src/game/deck.js
+++
@@ -1,6 +0,0 @@
-import { Jersey, Guernsey } from "./card";
-
-export function create_player_deck() {
-  let deck = [];
-  return deck.concat(Array(5).fill(Jersey()), Array(3).fill(Guernsey()), Array(3). fill);
-}
