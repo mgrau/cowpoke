2dbc0c54e71ea84b9312de358735aac1fb9e7ee5
Matt Grau <matt.grau@gmail.com>
2019-04-03 02:51:22 +0200

    move app index index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import { Client } from "boardgame.io/react";
+import { Cowpoke } from "./game/game";
+import CowpokeBoard from "./board/board";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const CowpokeClient = Client({
+  numPlayers: 3,
+  game: Cowpoke,
+  board: CowpokeBoard,
+  debug: false
+});
+
+ReactDOM.render(
+  <div>
+    <CowpokeClient playerID="0" />
+  </div>,
+  document.getElementById("root")
+);
